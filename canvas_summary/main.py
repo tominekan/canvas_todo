65a246b668e6f38e9c753878b12adbf7b4e63767
Tomi Adenekan <70237683+tominekan@users.noreply.github.com>
2022-11-07 20:26:06 -0800

    Update main.py

--- canvas_summary/main.py
+++ canvas_summary/main.py
@@ -1,7 +1,7 @@
 from simplecanvasuser import SimpleCanvasUser
 from pprint import pprint
 API_URL = "https://smuhsd.instructure.com/"
-API_KEY = "3533~cFOiR8PTZ1fkhvmHpUzf3a4Q4ukv98Mmwe1J8cFnHboPyuHxwqtuzZDmlxh0cVdj"
+API_KEY = "tonka"
 
 user = SimpleCanvasUser(API_URL, API_KEY)
 pprint(user.active_courses)
