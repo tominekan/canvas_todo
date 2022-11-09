aa52f1b6f531559f9af038be882014fd80bf8ce6
Tomi Adenekan <tominekan@outlook.com>
2022-11-08 21:14:40 -0800

    :sparkles: Added todoist integration
    yess

--- canvas_summary/main.py
+++ canvas_summary/main.py
@@ -1,8 +1,9 @@
 from simplecanvasuser import SimpleCanvasUser
 from pprint import pprint
-API_URL = "https://smuhsd.instructure.com/"
-API_KEY = "tonka"
+from os import environ
+CANVAS_API_URL = "https://smuhsd.instructure.com/"
+CANVAS_API_KEY = environ.get("CANVAS_KEY")
 
-user = SimpleCanvasUser(API_URL, API_KEY)
+user = SimpleCanvasUser(CANVAS_API_URL, CANVAS_API_KEY)
 pprint(user.active_courses)
 pprint(user.current_hwk)
--- canvas_summary/todoist.py
+++ canvas_summary/todoist.py
@@ -1,11 +1,75 @@
 from todoist_api_python.api import TodoistAPI
+from os import environ
 
-# Fetch tasks synchronously
-def get_tasks_sync():
-    api = TodoistAPI("my token")
-    try:
-        tasks = api.get_tasks()
-        print(tasks)
-    except Exception as error:
-        print(error)
+class TodoistProject:
+    def __init__(self, api_key: str, project_name: str, debug=False):
+
+        """
+        TodoistProject, this simulates a single todoist project,
+        which is all I need for this project, you can only add or get tasks
+        which again, is all I need.
+        """
+
+
+        self.api = TodoistAPI(api_key)
+        self.debugOn = debug
+        self.project_id = self.__selectProjects(project_name)
+        if (self.project_id == -1):
+            raise Exception("Data can't be extracted")
+        
+    
+    def __debugPrint(self, *args):
+        if (self.debugOn):
+            print(args)
+
+    def __selectProjects(self, project_name):
+        try:
+
+            for project in self.api.get_projects():
+                if (project.name == project_name):
+                    self.__debugPrint(f"{project.name} MATCH!")
+                    return project.id
+                self.__debugPrint(f"{project.name} not a match")
+        except Exception as e:
+            self.__debugPrint("Error: ")
+            self.__debugPrint(e)
+            return -1
+
+    
+    def getProjectTasks(self):
+        """
+        Gets all the tasks in a specific project
+        returns -1 if not successful
+        """
+
+        try:
+            self.__debugPrint(f"Getting Tasks for project: {self.project_id}")
+            return self.api.get_tasks(project_id=self.project_id)
+        except Exception as e:
+            self.__debugPrint("Error: ")
+            self.__debugPrint(e)
+            return -1
+    
+    def addTask(self, content, due_date):
+
+        """
+        Add a single task to a specific project, takes the name of the task and the due date
+        returns -1 if not successful
+        """
+
+
+        try:
+            self.__debugPrint(f"Adding task \"{content}\": due {due_date} to project: {self.project_id}")
+            self.api.add_task(content=content, due_string=due_date, due_lang="en", project_id=self.project_id)
+        except Exception as e:
+            self.__debugPrint("Error: ")
+            self.__debugPrint(e)
+            return -1
+
+api = TodoistProject(
+    api_key=environ.get("TODOIST_KEY"),
+    project_name="The Drill"
+)
+
+api.addTask(content="Tonka", due_date="tomorrow")
 
